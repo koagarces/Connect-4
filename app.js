2c78a90a6a34ab531c19f77345366a32bbcfc7eb
Koa Garces <koajgarces@yahoo.com>
2022-03-08 08:52:31 -0800

    changed grid by using flex grid

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// const size = 7
-// const grid = new Grid()
-// for (let x = 0; x < size; x++) {
-//     for (let y = 0; y < size; y++) {
-//         grid.addCell(x, y)
-//     }
-// }
+const slots = document.getElementById('grid');
+
+
+slots.forEach(slot => {
+
+
+});
+
