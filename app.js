41e0783056fa7670dfc8f9375455fc1ee951ffa2
Koa Garces <koajgarces@yahoo.com>
2022-03-09 22:22:50 -0800

    got board to work

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     let slots = document.querySelectorAll('#grid div');
     let player = document.getElementById('player');
-    let playerTurn = 'player 1';
+    let playerTurn = 'Player 1';
 
     for (let i = 0; i < slots.length; i++) {
         slots[i].onclick = () => {
 
-            if (playerTurn === 'player 1') {
+            if (playerTurn === 'Player 1') {
                 if (slots[i + 6].classList.contains('bottom')) {
                     slots[i].classList.add('bottom');
                     slots[i].classList.add('player1');
-                    playerTurn = 'player 2';
+                    playerTurn = 'Player 2';
                     player.innerHTML = playerTurn;
                 }
 
-            } else if (playerTurn === 'player 2') {
+            } else if (playerTurn === 'Player 2') {
                 if (slots[i + 6].classList.contains('bottom')) {
                     slots[i].classList.add('bottom');
                     slots[i].classList.add('player2');
-                    playerTurn = 'player 1'
+                    playerTurn = 'Player 1'
                     player.innerHTML = playerTurn
-                } else
-                    alert('no');
+                }
+            } else
+                console.log(alert('no'));
 
 
-            }
+
         }
     }
 })
