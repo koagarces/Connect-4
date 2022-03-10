b56de38fe140893c606eea2e719bc5633cb74fa4
Koa Garces <koajgarces@yahoo.com>
2022-03-09 22:03:12 -0800

    board is functioning, except bottom row

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     let slots = document.querySelectorAll('#grid div');
-
-
+    let player = document.getElementById('player');
+    let playerTurn = 'player 1';
 
     for (let i = 0; i < slots.length; i++) {
         slots[i].onclick = () => {
-            alert(`this is ${[i]}`)
+
+            if (playerTurn === 'player 1') {
+                if (slots[i + 6].classList.contains('bottom')) {
+                    slots[i].classList.add('bottom');
+                    slots[i].classList.add('player1');
+                    playerTurn = 'player 2';
+                    player.innerHTML = playerTurn;
+                }
+
+            } else if (playerTurn === 'player 2') {
+                if (slots[i + 6].classList.contains('bottom')) {
+                    slots[i].classList.add('bottom');
+                    slots[i].classList.add('player2');
+                    playerTurn = 'player 1'
+                    player.innerHTML = playerTurn
+                } else
+                    alert('no');
+
+
+            }
         }
     }
-
 })
